c8832d29e75376bc1957a273aa31d5a2082882f5
MishalAlMazidi <mishal.mazidi.video@gmail.com>
2017-10-30 11:31:17 +0000

    Created Creature java file, and empty Main class

---
+++ src/Creature.java
@@ -0,0 +1,90 @@
+/**
+ * Created by k1631973 on 30/10/17.
+ */
+public class Creature {
+    int energy, fullness, happiness;
+    String name;
+
+    public Creature(String name) {
+        energy = 15;
+        fullness = 15;
+        happiness = 15;
+
+        this.name = name;
+    }
+
+    public void eat(){
+        if (fullness >= 12){
+            print ( "Woops, " + this.getName() + " is already full!");
+        }
+        else {
+            fullness += 8;
+            happiness -= 6;
+            energy -= 4;
+
+            if(fullness >15){ fullness = 15;}
+
+            print("Yum " + this.getName() + " That was tasty!");
+        }
+
+
+    }
+
+    public void sleep(){
+        if (energy >= 12){
+            print ( "Woops, " + this.getName() + " is that sleepy!");
+        }
+        else {
+            energy += 8;
+            happiness -= 5;
+            fullness -= 5;
+
+            if(energy >15){ energy = 15;}
+
+            print("*Yawn* " + this.getName() + " That was a good nap!");
+        }
+
+
+    }
+
+    public void play(){
+
+            happiness += 8;
+            energy -= 4;
+            fullness -= 6;
+
+            if(happiness >15){ happiness = 15;}
+
+        print("That was so much fun! Let's do that again!");
+        }
+
+    public String checkstatus() {
+        String status = "";
+
+        if (happiness <= 6) {
+            status = "Oh. " + this.getName() + "is really sad!\n";
+
+        }
+        if (energy <= 6) {
+            status = "Oh. " + this.getName() + "is really tired!\n";
+
+        }
+
+        if (fullness <= 6) {
+            status = "Oh. " + this.getName() + "is super hungry!\n";
+
+        }
+
+        return status;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void print(String s){
+        System.out.print(s);
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by k1631973 on 30/10/17.
+ */
+public class Main {
+    public static void main(String args[])
+
+    {
+        System.out.println("Hello world");
+    }
+}
